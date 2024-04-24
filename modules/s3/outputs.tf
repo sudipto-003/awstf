d83b5f6e6c6465b1da8e3b9bf563219da088830f
sudipto-003 <sudipto00003@gmail.com>
2024-04-25 04:21:52 +0600

    terraform fmt

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,9 +1,9 @@
 output "notun_baltir_name" {
-    value = var.baltir_nam != "" ? one(aws_s3_bucket.balti_nam_thik_ache[*].id) : one(aws_s3_bucket.balti_nam_thik_nai[*].id)
-    description = "New bucket created by the module"
+  value       = var.baltir_nam != "" ? one(aws_s3_bucket.balti_nam_thik_ache[*].id) : one(aws_s3_bucket.balti_nam_thik_nai[*].id)
+  description = "New bucket created by the module"
 }
 
 output "notun_baltir_arn" {
-    value = var.baltir_nam != "" ? one(aws_s3_bucket.balti_nam_thik_ache[*].arn) : one(aws_s3_bucket.balti_nam_thik_nai[*].arn)
-    description = "ARN of new bucket created by the module"
+  value       = var.baltir_nam != "" ? one(aws_s3_bucket.balti_nam_thik_ache[*].arn) : one(aws_s3_bucket.balti_nam_thik_nai[*].arn)
+  description = "ARN of new bucket created by the module"
 }
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,24 +1,24 @@
 variable "baltir_nam" {
-    type = string
-    default = ""
+  type    = string
+  default = ""
 }
 
 variable "baltir_prefix_name" {
-    type = string
-    default = ""
+  type    = string
+  default = ""
 }
 
 variable "age_balti_khali_pore_destroy" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
 
 variable "balti_tala_dewa" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
 
 variable "baltir_tag" {
-    type = map(string)
-    default = {}
+  type    = map(string)
+  default = {}
 }
